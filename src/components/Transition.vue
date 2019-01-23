608b9130775c6eae58c1b494f1bdc3296b8f7c41
Friederike Schwager <friederike.schwager@rwth-aachen.de>
2019-01-23 13:09:16 +0100

    add marker to transition

--- src/components/Transition.vue
+++ src/components/Transition.vue
@@ -1,8 +1,24 @@
 <template>
-    <path
-        d="M125 170 L125 300"
-        stroke="green"
-        stroke-width="3" />
+    <g>
+        <defs>
+            <marker
+                id="markerArrow"
+                markerWidth="13"
+                markerHeight="13"
+                refX="2"
+                refY="6"
+                orient="auto">
+                <path
+                    d="M2,2 v8 l8,-4z "
+                    style="fill: #000000;" />
+            </marker>
+        </defs>
+        <path
+            d="M125 170 L125 275"
+            stroke="green"
+            stroke-width="3"
+            style="marker-end: url(#markerArrow);" />
+    </g>
 </template>
 
 <script>
@@ -14,11 +30,6 @@ export default {
 </script>
 
 <style scoped>
-.state {
-    fill: red;
-    stroke: black;
-    stroke-width: 5;
-    opacity: 0.5;
-}
+
 </style>
 
