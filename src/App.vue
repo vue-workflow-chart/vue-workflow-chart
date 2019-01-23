ec4cd1645d68b15c9a12241e4feacf5d681c1e9c
Johannes Roos <roos@modell-aachen.de>
2019-01-23 09:28:17 +0100

    add simple svg example

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
