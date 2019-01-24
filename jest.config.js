98d939bd51fb634f348c7655b9c16bb1eb8c7c8f
marcel-postel <marcel@postel-privat.de>
2019-01-24 15:38:25 +0100

    fix bugs

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,7 @@ module.exports = {
         'jest-serializer-vue',
     ],
     testMatch: [
-        '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+        '**/test/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
     ],
     testURL: 'http://localhost/',
 };
