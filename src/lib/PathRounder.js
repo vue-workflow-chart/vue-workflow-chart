87379ad8a0cf4879f014892c07f8bc78acdfd5dc
Friederike Schwager <friederike.schwager@rwth-aachen.de>
2019-02-25 14:21:48 +0100

    change radius for curves

--- src/lib/PathRounder.js
+++ src/lib/PathRounder.js
@@ -12,14 +12,51 @@ export default class Path {
 
     setPath(transitionPath) {
         const points = cloneDeep(transitionPath);
-        let start = points.shift();
-        let path = `M${start.x} ${start.y}`;
+        const radius = 12;
+        let p1;
+        let p2 = points.shift();
 
-        for (const point of points) {
-            path += ` L${point.x} ${point.y}`;
+        let v;
+        let norm;
+        let q;
+        let r;
+
+        let path = `M${p2.x} ${p2.y}`;
+
+        do {
+            p1 = p2;
+            p2 = points.shift();
+
+            v = { x: (p2.x-p1.x),
+                y: (p2.y-p1.y),
+            };
+            norm = Math.sqrt(v.x * v.x + v.y*v.y);
+            v.x /= norm;
+            v.y /= norm;
+
+            if (norm < 2*radius) {
+                q = { x: (p1.x + norm/2 * v.x),
+                    y: (p1.y + norm/2 * v.y),
+                };
+                r = { x: (p2.x - norm/2 * v.x),
+                    y: (p2.y - norm/2 * v.y),
+                };
+
+            } else {
+                q = { x: (p1.x + radius * v.x),
+                    y: (p1.y + radius * v.y),
+                };
+                r = { x: (p2.x - radius * v.x),
+                    y: (p2.y - radius * v.y),
+                };
+            }
+
+            path += ` Q${p1.x} ${p1.y} ${q.x} ${q.y}`;
+            path += ` L${r.x} ${r.y}`;
+        } while (points.length > 0);
+        if ((r.x !== p2.x) && (r.y !== p2.y)) {
+            path += ` L${p2.x} ${p2.y}`;
         }
-
         this.path = path.trim();
     }
 }
-
