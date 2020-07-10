aea2b4f5c9b3f6bc4b9694f6d20490e8c97049aa
Marcel Postel <marcelpostel.github@gmail.com>
2020-07-10 12:04:44 +0200

    Housekeeping (#20)
    * Remove obsolete npmignore file
    
    * Add single quotes to eslintrc
    
    * Update gitignore file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
-    root: true,
+    'root': true,
 
-    env: {
-        node: true,
+    'env': {
+        'node': true,
     },
 
-    rules: {
+    'rules': {
         'brace-style':                  [1],
         'callback-return':              [2, ['callback', 'cb', 'next', 'done', 'proceed']],
-        camelcase:                    [1, { properties: 'always' }],
+        'camelcase':                    [1, { 'properties': 'always' }],
         'comma-dangle':                 ['warn', 'always-multiline'],
         'comma-style':                  [2, 'last'],
-        curly:                        [2],
+        'curly':                        [2],
         'eol-last':                     [1],
-        eqeqeq:                       [2, 'smart'],
+        'eqeqeq':                       [2, 'smart'],
         'handle-callback-err':          [2],
-        indent:                       [2, 4, { SwitchCase: 1 }],
+        'indent':                       [2, 4, { 'SwitchCase': 1 }],
         'linebreak-style':              [2, 'unix'],
-        'no-console':                   ['error', { allow: ['warn','error','log'] }],
+        'no-console':                   ['error', { 'allow': ['warn','error','log'] }],
         'no-mixed-spaces-and-tabs':     [2, 'smart-tabs'],
         'no-return-assign':             [2, 'always'],
         'no-sequences':                 [2],
         'no-trailing-spaces':           [2],
         'no-undef':                     [0],
         'no-unexpected-multiline':      [1],
-        'no-unused-vars':               ['warn', { args: 'after-used' }],
+        'no-unused-vars':               ['warn', { 'args': 'after-used' }],
         'no-var':                       [1],
         'one-var':                      [2, 'never'],
         'object-curly-spacing':         [1, 'always'],
-        semi:                         [2, 'always'],
-        'vue/html-closing-bracket-newline': ['error', { singleline: 'never', multiline: 'never' }],
+        'semi':                         [2, 'always'],
+        'vue/html-closing-bracket-newline': ['error', { 'singleline': 'never', 'multiline': 'never' }],
         'vue/html-indent':              ['error', 4],
         'vue/attribute-hyphenation': 'off',
     },
 
-    parserOptions: {
-        parser: 'babel-eslint',
+    'parserOptions': {
+        'parser': 'babel-eslint',
     },
 
     'extends': [
